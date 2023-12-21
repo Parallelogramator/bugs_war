0a7fae092e626cc4a44ba15634034522a05de44c
riebz <151185012+riebz@users.noreply.github.com>
2023-12-22 01:00:28 +0300

    персонаж управляется игроком вместо жука, персонаж не переворачивается когда идет вниз Bugs.py

--- Bugs.py
+++ Bugs.py
@@ -11,10 +11,10 @@ win_width, win_height = 800, 600
 win = pygame.display.set_mode((win_width, win_height))
 
 # Загрузка изображений персонажа
-character_right = pygame.image.load('жуг.png')
+character_right = pygame.image.load('персонаж облаченный зеленый.png')
 character_left = pygame.transform.flip(character_right, True, False)
-character_up = pygame.transform.rotate(character_right, -90)
-character_down = pygame.transform.rotate(character_right, 90)
+character_up = pygame.transform.rotate(character_right, 0)
+character_down = pygame.transform.rotate(character_right, 0)
 
 # Загрузка изображений персонажа и жука
 character = character_right
